fc04c688b3dbf993c159b6cf2d1c755a7747beda
Adrian Ancher <Adrian.ancher@gmail.com>
2026-09-09 11:14:30 +0200

    hello world project

---
+++ Chapter3/helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        // The Main method is the starting point of the application
+        static void Main(string[] args)
+        {
+            int number1 = 15;
+            int number2 = 25;
+            int sum = number1 + number2;
+
+            Console.WriteLine($"The sum of {number1} and {number2} is: {sum}");
+        }
+    }
+}
